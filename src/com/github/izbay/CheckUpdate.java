8affd332cd40df24a6473eac0980f773945297cb
izbay <jacobmartinek@yahoo.com>
2013-11-18 12:52:42 -0500

    CheckUpdate with Bukkit API

--- src/com/github/izbay/CheckUpdate.java
+++ src/com/github/izbay/CheckUpdate.java
@@ -1,74 +1,134 @@
 package com.github.izbay;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
- 
-public class CheckUpdate implements Runnable {
-	
-	private Logger logger;
-	private String version;
-	
-	/** Constructor.
-	 * @param plugin The instance of this plugin.
-	 */
-	public CheckUpdate(StablemasterPlugin plugin){
-		logger = plugin.getLogger();
-		version = plugin.getDescription().getVersion();
-	}
-	
+
+import org.bukkit.plugin.Plugin;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+/**
+ * This class is a barebones example of how to use the BukkitDev ServerMods API
+ * to check for file updates. <br>
+ * See the README file for further information of use.
+ */
+public class CheckUpdate {
+
+	// The project's unique ID
+	private final int projectID;
+	private final Plugin plugin;
+
+	// An optional API key to use, will be null if not submitted
+	private final String apiKey;
+
+	// Keys for extracting file information from JSON response
+	private static final String API_NAME_VALUE = "name";
+
+	// Static information for querying the API
+	private static final String API_QUERY = "/servermods/files?projectIds=";
+	private static final String API_HOST = "https://api.curseforge.com";
+
 	/**
-	 * Check for an update in a new thread. This is to prevent slowdown at boot while parsing HTML.
-	 * Promts the user if a new version exists by checking the page on BukkitDEV.
+	 * Check for updates anonymously (keyless)
+	 * 
+	 * @param projectID
+	 *            The BukkitDev Project ID, found in the "Facts" panel on the
+	 *            right-side of your project page.
 	 */
-	public void run(){
-		URL url;
-		URLConnection yc;
-		Scanner in = null;
+	public CheckUpdate(Plugin plugin, int projectID) {
+		this(plugin, projectID, null);
+	}
+
+	/**
+	 * Check for updates using your Curse account (with key)
+	 * 
+	 * @param projectID
+	 *            The BukkitDev Project ID, found in the "Facts" panel on the
+	 *            right-side of your project page.
+	 * @param apiKey
+	 *            Your ServerMods API key, found at
+	 *            https://dev.bukkit.org/home/servermods-apikey/
+	 */
+	public CheckUpdate(Plugin plugin, int projectID, String apiKey) {
+		this.projectID = projectID;
+		this.apiKey = apiKey;
+		this.plugin = plugin;
 		
-		logger.log(Level.INFO, "Checking for Stablemaster updates...");
-		// Attempt to connect to the BukkitDev site.
+		query();
+	}
+
+	/**
+	 * Query the API to find the latest approved file's details.
+	 */
+	public void query() {
+		URL url = null;
+
 		try {
-			url = new URL("http://dev.bukkit.org/server-mods/stablemaster/files/");
-			yc = url.openConnection();
-			in = new Scanner(new InputStreamReader(yc.getInputStream()));
-		} catch (IOException e) {
-			logger.log(Level.SEVERE, "**************************************");
-			logger.log(Level.SEVERE, "UNABLE TO CONFIRM PLUGIN IS UP TO DATE");
-			logger.log(Level.SEVERE, "PESTER IZBAY ON BUKKITDEV ABOUT THIS!");
-			logger.log(Level.SEVERE, "**************************************");
+			// Create the URL to query using the project's ID
+			url = new URL(API_HOST + API_QUERY + projectID);
+		} catch (MalformedURLException e) {
+			// There was an error creating the URL
+
+			e.printStackTrace();
 			return;
 		}
-    	
-    	// Scan for the first instance of stablemaster version on the page.
-		String inputLine = null;
-	    while(in.hasNext()){    
-	    	inputLine = in.next();
-	    	if (inputLine != null && inputLine.contains("stablemaster-v")){
-	    		break;
-	    	}
-	    }
-	    in.close();
-	    
-	    // Format the string for conversion to a float.
-	    String breakdown[] = inputLine.split("-");
-	    String formattedVersion = breakdown[3].substring(1) + "." + breakdown[4].substring(0, 2);
-	    
-		Float thisVersion = Float.parseFloat(version);
-	    Float currVersion = Float.parseFloat(formattedVersion);
-	    
-		if (currVersion > thisVersion){
-			logger.log(Level.SEVERE, "*****************************************");
-			logger.log(Level.SEVERE, "YOUR VERSION IS NO LONGER THE MOST RECENT");
-			logger.log(Level.SEVERE, "GET THE LATEST BUILD, " + currVersion + ", ON BUKKITDEV!");
-			logger.log(Level.SEVERE, "bit.ly/stablemaster");
-			logger.log(Level.SEVERE, "*****************************************");
-		} else {
-			logger.log(Level.INFO, "This version is up to date. Good job!");
+
+		try {
+			// Open a connection and query the project
+			URLConnection conn = url.openConnection();
+
+			if (apiKey != null) {
+				// Add the API key to the request if present
+				conn.addRequestProperty("X-API-Key", apiKey);
+			}
+
+			// Add the user-agent to identify the program
+			conn.addRequestProperty("User-Agent",
+					"ServerModsAPI-Example (by Gravity)");
+
+			// Read the response of the query
+			// The response will be in a JSON format, so only reading one line
+			// is necessary.
+			final BufferedReader reader = new BufferedReader(
+					new InputStreamReader(conn.getInputStream()));
+			String response = reader.readLine();
+
+			// Parse the array of files from the query's response
+			JSONArray array = (JSONArray) JSONValue.parse(response);
+
+			if (array.size() > 0) {
+				// Get the newest file's details
+				JSONObject latest = (JSONObject) array.get(array.size() - 1);
+
+				// Get the version's title
+				String versionName = (String) latest.get(API_NAME_VALUE);
+				double newVersion = Double.parseDouble(versionName.substring(versionName.length()-4));
+				
+				// Get the version of the currently running plugin
+				double thisVersion = Double.parseDouble(plugin.getDescription().getVersion());
+
+				if(thisVersion >= newVersion){
+					System.out.println("This version is up to date. Good job!");
+				} else {
+					System.out.println("*****************************************");
+					System.out.println("YOUR VERSION IS NO LONGER THE MOST RECENT");
+					System.out.println("GET THE LATEST BUILD, v" + newVersion + ", ON BUKKITDEV!");
+					System.out.println("bit.ly/stablemaster");
+					System.out.println("*****************************************");
+				}
+			} else {
+				System.out.println("There are no files for this project");
+			}
+		} catch (IOException e) {
+			// There was an error reading the query
+
+			e.printStackTrace();
+			return;
 		}
 	}
 }
